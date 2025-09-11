eb2a48497595d0c203a69b25f0e6b23217da9389
Victor Kweyu <wkweyu@yahoo.co.uk>
2025-09-11 18:01:42 +0300

    Resolve merge conflict

--- vite.config.ts
+++ vite.config.ts
@@ -7,21 +7,11 @@ import { componentTagger } from "lovable-tagger";
 export default defineConfig(({ mode }) => ({
   server: {
     host: "::",
-<<<<<<< HEAD
     port: 5173,
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(
     Boolean
   ),
-=======
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
->>>>>>> ac7828269a6664fd6413e1756f459d58d5f507b1
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
